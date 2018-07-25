86f468477cc853b49dee5f4d20721a86f0f95a9e
Liu Yanbo <yanbo.liu@ucarinc.com>
2018-07-25 09:13:36 +0800

    Initial commit

---
+++ lib/TextPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('TextPage'),
+      ),
+      body: new Text('Text Page'),
+    );
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'TextPage.dart';
+import 'ImagePage.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new FeatureList(),
+      theme: new ThemeData(
+        primaryColor: Colors.green,
+      ),
+    );
+  }
+}
+
+class FeatureList extends StatefulWidget {
+  @override
+  createState() => new _FeatureListState();
+}
+
+class _FeatureListState extends State<FeatureList> {
+  var list = ['文本', '图片'];
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('功能列表'),
+      ),
+      body: new ListView(
+          children: list.map((String name) {
+        return new ListTile(
+            leading: new Icon(Icons.school),
+            title: new Text(name),
+            onTap: () {
+              goNext(name);
+            });
+      }).toList()),
+    );
+  }
+
+  void goNext(page) {
+    switch (page) {
+      case '文本':
+        jumpPage(new TextPage());
+        break;
+      case '图片':
+        jumpPage(new ImagePage());
+        break;
+    }
+  }
+
+  void jumpPage(StatelessWidget widget) {
+    Navigator.of(context).push(
+          new MaterialPageRoute(builder: (context) => widget),
+        );
+  }
+}
